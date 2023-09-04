e15cc288817599c7364df38d821fac51e3339539
Gtamazashvili <118844834+Gtamazashvili@users.noreply.github.com>
2023-09-04 20:31:24 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('randomGame').addEventListener('click', function() {
+    window.location.href = 'https://gtamazashvili.github.io/Calculator/';
+});
+document.getElementById('calculator').addEventListener('click', function() {
+    window.location.href = 'https://gtamazashvili.github.io/RandomGame/';
+});
+document.getElementById('facebookIcon').addEventListener('click',function(){
+    window.location.href ='https://www.facebook.com/giorgi.tamazashvili.169';
+});
+document.getElementById('instagramIcon').addEventListener('click',function(){
+    window.location.href ='https://www.instagram.com/giorgitamazashvili__/';
+});
+document.getElementById('githubIcon').addEventListener('click',function(){
+    window.location.href ='https://github.com/Gtamazashvili';
+});
